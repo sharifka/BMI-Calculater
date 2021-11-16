9cee9c864c4356360cb9632737eb5a507a26efce
sharifka <abdifatahsharif790@gmail.com>
2021-11-16 08:07:18 -0700

    first commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -36,6 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text('BMI Calculator'),
         centerTitle: true,
       ),
